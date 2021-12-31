587119d71cf7a09959a8b3d0375d74212d99394c
zolateklu <zolateklu@gmail.com>
2021-12-31 17:30:55 +0300

    navbar added

---
+++ src/components/Navbar.vue
@@ -0,0 +1,50 @@
+<template>
+    <div>
+        <div>
+            <nav class="navbar">
+                <div class="logo">
+                    <h1>RoBlog</h1>
+                </div>
+                <div class="menu">
+                    <h3>Home</h3>
+                    <h3>Blogs</h3>
+                    <h3>Login</h3>
+                </div>
+            </nav>
+        </div>
+    </div>
+
+</template>
+
+<script>
+export default {
+    name: 'Navbar',
+    data() {
+        return {
+            msg: 'Welcome to Your Vue.js App'
+        }
+    }
+}
+</script>
+
+<style>
+.navbar {
+    text-align: center;
+    margin-top: -10px;
+    color: #fff;
+}
+.logo {
+    float: left;
+    font: 1em sans-serif;
+    margin-top: -1rem;
+}
+.menu {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    float: right;
+}
+.menu h3 {
+    margin: 10px;
+}
+</style>
